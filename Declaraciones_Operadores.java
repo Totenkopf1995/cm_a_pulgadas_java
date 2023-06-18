f336c7dae7b830a17043dae01eac4f1a931734cc
Totenkopf1995 <121690654+Totenkopf1995@users.noreply.github.com>
2023-06-18 16:09:05 -0500

    Add files via upload

---
+++ Declaraciones_Operadores.java
@@ -0,0 +1,19 @@
+public class Declaraciones_Operadores {
+    
+    public static void main(String[] args) {
+
+        double cm,resultado;
+        
+        final double apulgadas;
+
+        apulgadas= 2.54;
+
+        cm = 6;
+
+        resultado = cm / apulgadas;
+
+        System.out.println("En " + cm + " cm hay " + resultado + " pulgadas");
+
+    }
+
+}
